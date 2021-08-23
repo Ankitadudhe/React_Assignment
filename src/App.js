c71f8acef215bf458620229a80b29d46c99b17f3
Ankitadudhe <ankitadudhe95@gmail.com>
2021-08-23 22:43:46 +0530

    added assignment9

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-import React, { useCallback, useState } from 'react';
-import Button from './components/UI/Button/Button'
-import './App.css';
-import Demo from './components/Demo'
+import UserFinder from './components/UserFinder';
+
 function App() {
-  const [showParagragh,setShowParagragh]=useState(false);
-  const [allowToggle,setAllowToggle]=useState(false);
-
-  const onClickHandler=useCallback(()=>{
-    if(allowToggle){
-      setShowParagragh(prevshowParagragh=>!prevshowParagragh)
-      }
-  },[allowToggle]);
-
-  const allowToggleHandler=()=>{
-    setAllowToggle(true);
-  }
   return (
-    <div className="app">
-      <h1>Hi there!</h1>
-     {/* {showParagragh && <p>This is new</p>} */}
-     <Demo show={showParagragh}/>
-     {/* React.memo() */}
-     {/* <Demo show={false}/> */}
-     <Button onClick={allowToggleHandler}>Allows Toggle</Button>
-     <Button onClick={onClickHandler}>Toggle paragragh</Button>
+    <div>
+      <UserFinder />
     </div>
   );
 }
